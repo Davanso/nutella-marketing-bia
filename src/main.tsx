5ebd17ea32b862df6cf84a1d9b4989a65fdad44e
davanso <guipvp234@gmail.com>
2025-05-27 18:43:10 -0300

    feat: add initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import './index.css'
 import App from './App.tsx'
+import React from 'react'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
