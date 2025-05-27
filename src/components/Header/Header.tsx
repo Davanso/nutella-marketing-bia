2596116371e3978c2d2e4c2d8c953599ba04c756
davanso <guipvp234@gmail.com>
2025-05-27 20:36:26 -0300

    feat ✨: finished header styles and add social links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,19 +6,25 @@ import {
     FaYoutube,
     FaInstagram,
 } from "react-icons/fa6";
-import { FaSearch } from "react-icons/fa";
+
 
 
 const Header: React.FC = () => {
     return (
         <header className="header-container">
             <div className="header-icons">
-                <FaFacebookF color={"blue"}/>
-                <FaXTwitter />
-                <FaYoutube color={"red"}/>
-                <FaInstagram color={"#d901b3"}/>
-                |
-                <FaSearch />
+                <a href="https://www.facebook.com/Nutella/photos/?locale=fr_FR&_rdr"  target="_blank" rel="noopener noreferrer">
+                    <FaFacebookF color={"blue"}/>
+                </a>
+                <a href="https://x.com/nutellaglobal" target="_blank" rel="noopener noreferrer">
+                    <FaXTwitter />
+                </a>
+                <a href="https://www.youtube.com/@nutellabrasil3671" target="_blank" rel="noopener noreferrer">
+                    <FaYoutube color={"red"}/>
+                </a>
+                <a href="https://www.instagram.com/nutella/" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram color={"#d901b3"}/>
+                </a>
             </div>
         </header>
     );
